d6ea59b6253336574083af3bf1c4433c12e90625
Evaggelou <alexandrose95@gmail.com>
2021-03-04 13:17:44 +0200

    Replace related values of 2 with 1

--- data/process_data.py
+++ data/process_data.py
@@ -51,6 +51,9 @@ def clean_data(df):
         # convert column from string to numeric
         categories[column] = categories[column].astype("int")
 
+    # Replace related values of 2 with 1
+    categories['related'] = categories.related.replace(2,1)
+
     # drop the original categories column from `df`
     df.drop('categories', axis = 1, inplace = True)
     
